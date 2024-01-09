201f9f4bf7d30f3371e7d82600b7edd985faebfc
Florin12er <142166195+Florin12er@users.noreply.github.com>
2024-01-09 07:39:14 +0100

    added more tests

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
 function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
+function divide(a, b) {
+  return a / b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function checkIfCapitalized(str) {
+  return str.charAt(0).toUpperCase() === str.charAt(0);
+}
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+function caesarCipher(str, shift) {
+  return str
+    .split("")
+    .map((char) => {
+      const code = char.charCodeAt();
+      if (code >= 65 && code <= 90) {
+        const shiftedCode = ((code - 65 + shift) % 26) + 65;
+        return String.fromCharCode(shiftedCode);
+      } else if (code >= 97 && code <= 122) {
+        const shiftedCode = ((code - 97 + shift) % 26) + 97;
+        return String.fromCharCode(shiftedCode);
+      } else {
+        return char;
+      }
+    })
+    .join("");
+}
+function analyzeArray(arr) {
+  return {
+    average: arr.reduce((a, b) => a + b, 0) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+}
+const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+object ==
+  {
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  };
+
+module.exports = {
+  sum: sum,
+  checkIfCapitalized: checkIfCapitalized,
+  reverseString: reverseString,
+  divide: divide,
+  multiply: multiply,
+  subtract: subtract,
+  caesarCipher: caesarCipher,
+  analyzeArray: analyzeArray,
+};
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,43 @@
-const sum = require("./index");
+const {
+  sum,
+  checkIfCapitalized,
+  reverseString,
+  divide,
+  multiply,
+  subtract,
+  caesarCipher,
+  analyzeArray,
+} = require("./index");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("check if capitalized", () => {
+  expect(checkIfCapitalized("Sebastian")).toBe(true);
+});
+test("reverse string", () => {
+  expect(reverseString("hello")).toBe("olleh");
+});
+test("divide", () => {
+  expect(divide(4, 2)).toBe(2);
+});
+test("multiply", () => {
+  expect(multiply(4, 2)).toBe(8);
+});
+test("subtract", () => {
+  expect(subtract(4, 2)).toBe(2);
+});
+test("caesar cipher", () => {
+  expect(caesarCipher("Beware the Ides of March", 5)).toBe(
+    "Gjbfwj ymj Nijx tk Rfwhm",
+  );
+});
+test("analyze array", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
